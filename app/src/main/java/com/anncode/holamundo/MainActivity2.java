cd7edf69dfa9370a37e94d25ba105d5b4de5dde4
Anahi Salgado <anahi.anndroid@gmail.com>
2015-10-08 21:13:15 -0500

    Hola Mundo Android

---
+++ app/src/main/java/com/anncode/holamundo/MainActivity2.java
@@ -0,0 +1,45 @@
+package com.anncode.holamundo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Bundle parametros = getIntent().getExtras();
+        String name =
+                parametros.getString(getResources().getResourceName(R.string.nombre_parametro_name));
+
+        TextView tvTexto = (TextView) findViewById(R.id.tvTexto2);
+        tvTexto.append(name);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main_activity2, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
